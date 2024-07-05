36de51493b01a16b9bc8bd1eb19087571cab5c35
Michela Di Simone <disimonemichela@gmail.com>
2024-07-05 18:15:59 +0200

    added methods

--- FileServer.py
+++ FileServer.py
@@ -3,8 +3,34 @@ import websockets
 import uuid
 from datetime import datetime
 
-connected_clients = {}
-chat_history = []
+connected_clients = {} # Dictionary to store the connected clients
+chat_history = [] # List to store the chat history
+
+async def send_to_all(message):
+    """
+    Broadcasts a message to all connected clients
+    """
+    # Loop through all connected clients and send the message
+    if connected_clients:
+        await asyncio.wait([client.send(message)] for client in connected_clients)
+
+
+# async def send_chat_history(websocket):
+#     """
+#     Sends the entire chat history to the newly connected client
+#     """
+#     for message in chat_history:
+#         await websocket.send(message)
+
+
+async def send_user_list():
+    """
+    Broadcasts the list of currently connected users to all clients
+    """
+    # Loop through all connected clients and send the user list
+    if connected_clients:
+        user_list = "USERS: " + "," + ",".join(connected_clients.values())
+        await asynco.wait([client.send(user_list_message)] for client in connected_clients)
 
 
 async def file_server(websocket, path):
