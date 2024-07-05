502213640924c1a9b2a0a546b1b33141bddd616c
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2024-07-05 18:12:47 +0200

    added comment to file_server method

--- FileServer.py
+++ FileServer.py
@@ -7,20 +7,34 @@ connected_clients = {}
 chat_history = []
 
 
+'''
+    In this method, we manage the principal logic of the file server:
+    - It handles the connection and communication with clients
+    - It maintains a list of connected clients and chat history
+    - It menage disconnection of clients
+
+    Args:
+        websocket: The WebSocket connection object for a client
+        path: The URL path requested by the client
+
+    Returns:
+        None
+'''
 async def file_server(websocket, path):
 
     try:
         await websocket.send("Welcome to the Shared File! Please enter your name:")
-        username = await websocket.recv()
+        username = await websocket.recv() # Wait for the client to send their username
 
         connected_clients[username] = websocket
         # await send_chat_history(websocket) # Send chat history to the new client
         join_message = f"{uuid.uuid4()}|System|{datetime.now().isoformat()}|{username} has joined the chat."
-        await send_to_all(join_message)
-        chat_history.append(join_message)
+        await send_to_all(join_message) # Send a message to all connected clients
+        chat_history.append(join_message) # Add the join_message to the chat history
 
-        await send_user_list()
+        await send_user_list() # Update user list to all connected clients
 
+        # Wait for messages from the client
         async for message in websocket:
             chat_message = f"{uuid.uuid4()}|{username}|{datetime.now().isoformat()}|{message}"
             chat_history.append(chat_message)
@@ -30,9 +44,10 @@ async def file_server(websocket, path):
         pass
 
     finally:
+        # Remove the client from the connected_clients dictionary
         leave_message = f"{uuid.uuid4()}|System|{datetime.now().isoformat()}|{username} has left the chat."
         connected_clients.pop(username)
-        await send_to_all(leave_message)
+        await send_to_all(leave_message) # Send a message to all connected clients that the user has left
         chat_history.append(leave_message)  
 
         await send_user_list()
