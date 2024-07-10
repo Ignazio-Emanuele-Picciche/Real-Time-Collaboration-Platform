ecd1b71b5e45bb0defcc870e922b060989c2748b
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2024-07-10 17:43:18 +0200

    Created class CRDT

--- FileServer.py
+++ FileServer.py
@@ -4,30 +4,12 @@ import json
 import os
 from datetime import datetime
 import websockets
+from CRDT import CRDT
 import difflib
 
 connected_clients = {} # Dictionary to store the connected clients
 FILE_PATH = "shared_file.txt" # Path to the shared file
 
-class CRDT:
-    """
-        This class implements the CRDT data structure for the shared file
-    """
-    def __init__(self):
-        self.document = ""
-        self.operations =[]
-    
-    # Method to generate a unique identifier for an operation
-    def apply_operation(self, operation):
-        if operation['type'] == 'insert':
-            self.document = self.document[:operation['index']] + operation['char'] + self.document[operation['index']:]
-        elif operation['type'] == 'delete':
-            self.document = self.document[:operation['index']] + self.document[operation['index'] +1:]
-            self.operations.append(operation)
-    
-    # Method to apply an operation to the document
-    def get_document(self):
-        return self.document
 
 crdt = CRDT()
 
